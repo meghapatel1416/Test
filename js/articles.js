0ae8a224595f61870a7260e9846c84491e9367e6
Your Name <you@example.com>
2024-03-18 21:10:53 +0530

    js test

---
+++ js/articles.js
@@ -0,0 +1,77 @@
+const ARTICLES = [
+    {
+      title: "A message to our customers",
+      upvotes: 12,
+      date: "2020-01-24",
+    },
+    {
+      title: "Alphabet earnings",
+      upvotes: 22,
+      date: "2019-11-23",
+    },
+    {
+      title: "Artificial Mountains",
+      upvotes: 2,
+      date: "2019-11-22",
+    },
+    {
+      title: "Scaling to 100k Users",
+      upvotes: 72,
+      date: "2019-01-21",
+    },
+    {
+      title: "the Emu War",
+      upvotes: 24,
+      date: "2019-10-21",
+    },
+    {
+      title: "What's SAP",
+      upvotes: 1,
+      date: "2019-11-21",
+    },
+    {
+      title: "Simple text editor has 15k monthly users",
+      upvotes: 7,
+      date: "2010-12-31",
+},
+  ];
+
+  const display = () =>{
+    let display = ARTICLES.map((v)=>{
+        return(`
+        
+        <tbody>
+        <tr>
+          
+          <th>${v.title}</th>
+          <td>${v.upvotes}</td>
+          <td>${v.date}</td>
+        </tr>
+        
+      </tbody>`)
+    });
+    document.getElementById("disp").innerHTML = display.join("");
+    
+  }
+
+
+
+display();
+
+
+
+const manageShort = (sorttype) => {
+    ARTICLES.sort((a,b)=>{
+        if(sorttype == 'sortUv'){
+            return b.upvotes - a.upvotes;
+        }
+        else if (sorttype == 'sortDate'){
+            return a.date - b.date;
+        }
+
+    })
+    display();
+
+}
+
+
